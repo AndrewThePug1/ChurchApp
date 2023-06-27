b689eb9f6b5952174b5ba5b9116ae6250cb8d9d3
AndrewThePug1 <andrewmondejarp@gmail.com>
2023-06-27 16:17:22 -0400

    Trying to fix GET /home erorr upon login

--- src/userRoutes.ts
+++ src/userRoutes.ts
@@ -59,7 +59,7 @@ router.post('/login', async (req, res) => {
   session.userId = user.id;
   session.role = user.role;
 
-  res.redirect('../views/home');
+  res.redirect('/home');
 });
 
 // Render the login form
